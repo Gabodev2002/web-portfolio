c23f5af5fe96db4d91ac98a82b399bcab94c6e72
Gabo Gonzalez <gabo.mgb6@gmail.com>
2025-07-01 10:19:03 -0700

    updates

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,24 @@
+'use client'
+
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 export const Header = () => {
+
+    const pathname = usePathname()
+    
+    const isActive = 'text-[#00FFFF]'
+
     return(
-        <nav className="w-full flex justify-between fixed px-4 pt-4 top-0 z-10">
-            <span>SQL</span>
+        <div className="w-full fixed top-0 z-10">
+            <nav className="w-full max-w-7xl flex justify-between px-4 pt-4 mx-auto">
+            <span>GaboDev.</span>
             <ul className="flex gap-8"> 
-                {/* <Link href={}></Link> */}
-                <li>Work</li>
-                <li>About</li>
-                <li>Contact</li>
+                <Link href={'/'} className={`${pathname === '/' && isActive}`}>Work</Link>
+                <Link href={'/about'} className={`${pathname === '/about' && isActive}`}>About</Link>
+                <Link href={'/contact'} className={`${pathname === '/contact' && isActive}`}>Contact</Link>
             </ul>
         </nav>
+        </div>
     )
 }
