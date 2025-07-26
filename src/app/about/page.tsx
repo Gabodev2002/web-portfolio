670412ca80742b372fa1fa8e59e13d9c32b555fd
Gabo Gonzalez <gabo.mgb6@gmail.com>
2025-07-25 20:32:09 -0700

    updates

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,27 +10,29 @@ import { SpotLightContainer } from '@/components/SpotLightContainer';
 
 export default function AboutPage () {
     return (
-       <section className={`${styles.sectionContainer}  mt-20 px-8 lg:px-12 max-w-6xl m-auto`}>
+       <section className={`${styles.sectionContainer}  mt-35 px-8 lg:px-12 max-w-6xl m-auto`}>
 
         <h1 className='text-4xl md:text-6xl z-10'>About <span className='text-[#00ffff]'> me</span></h1>
 
         <div className='grid grid-cols-1 md:grid-cols-2 gap-4 h-auto w-full max-w-7xl m-auto mt-10 md:mt-20'>
             
-            <SpotLightContainer>
 
-                <div className='flex flex-col gap-5  pt-5 '>
-                    <p className='leading-7 lg:'>I&apos;m a Front-End Developer with a passion for bringing designs to life. Give me a vision, and I&apos;ll transform it into a responsive, pixel-perfect reality. I&apos;m a quick learner and I love diving into new challenges to expand my skillset. I thrive in collaborative environments and enjoy working as part of a team.
-                    </p>
-                    
-                    <p>
-                        I&apos;m a self-taught developer, building my skills through platforms like Platzi and other online resources. Over the past year, I&apos;ve actively developed a range of personal projects, gaining practical experience and a strong foundation. I&apos;m ready to put my knowledge into production and contribute to impactful projects.
-                    </p>
+                <div className='flex flex-col items-center justify-center h-auto w-full'>
+                    <SpotLightContainer>
+                        <div className='flex flex-col items-center gap-5  pt-5 '>
+                            <p className='leading-7 lg:'>I&apos;m a Front-End Developer with a passion for bringing designs to life. Give me a vision, and I&apos;ll transform it into a responsive, pixel-perfect reality. I&apos;m a quick learner and I love diving into new challenges to expand my skillset. I thrive in collaborative environments and enjoy working as part of a team.
+                            </p>
+                            
+                            <p>
+                                I&apos;m a self-taught developer, building my skills through platforms like Platzi and other online resources. Over the past year, I&apos;ve actively developed a range of personal projects, gaining practical experience and a strong foundation. I&apos;m ready to put my knowledge into production and contribute to impactful projects.
+                            </p>
+                        </div>
+                    </SpotLightContainer>
                 </div>
 
-            </SpotLightContainer>
 
             <div className='w-full h-[450px] flex justify-center'>
-                <Image className='bg-cover rounded-2xl' style={maskImage} width={450} height={450} src="/fotocv.webp" alt="foto-cv" priority />
+                <Image className='bg-cover rounded-2xl' style={maskImage} width={400} height={450} src="/fotocv.webp" alt="foto-cv" priority />
             </div>
 
         </div>
@@ -52,8 +54,22 @@ export default function AboutPage () {
             </div>
         </div>
 
-        <div className='mt-10 md:mt-20' id='contact'>
-            <h1 className='text-4xl md:text-6xl z-10'>Contact</h1>
+        <div className='my-10 md:my-20'>
+            <h1 className='text-4xl md:text-5xl z-10'>Contact</h1>
+
+            <div className='flex justify-center items-center gap-4 mt-10 w-full '>
+               <SpotLightContainer>
+                    <div className='z-10'>
+                        <h2 className='pb-6'>If you would like to get in touch, feel free to reach out!</h2>
+                        <div className='flex flex-col items-center gap-5'>
+                            <p>Email: gabo.mgb6@gmail.com</p>
+                            <p>Tlf: +58 414-388-27-31</p>
+                            <p>Github: <a href="https://github.com/gabodev2002" target="_blank" rel="noopener noreferrer">github.com/gabodev2002</a></p>
+                        </div>
+                    </div>
+                </SpotLightContainer>
+            </div>
+
         </div>
 
        </section>
